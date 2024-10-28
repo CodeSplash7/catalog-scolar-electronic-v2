15d5d6afecd28cb090b32d6bb9ba58f41957f0e4
CodeSplash7 <codesplash7@gmail.com>
2024-10-28 19:04:59 +0200

    Attempt more to solve icon error

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import NextAuthSessionProvider from "@/lib/nextauth/next-auth-session";
 import "@/styles/styles.css";
-import Head from "next/head";
 
 export const metadata = {
   title: "Catalog Scolar Electronic",
@@ -9,12 +8,12 @@ export const metadata = {
     "Catalogul scolar al elevului, care il ajuta pentru vizualizarea rapida a notelor, absentelor si purtarii.",
   themeColor: "#000000",
   icons: [
-    { rel: "icon", sizes: "192x192", href: "/icons/icon-192x192.png" },
-    { rel: "icon", sizes: "512x512", href: "/icons/icon-512x512.png" },
+    { rel: "icon", sizes: "192x192", href: "/icon-192x192.png" },
+    { rel: "icon", sizes: "512x512", href: "/icon-512x512.png" },
     {
       rel: "apple-touch-icon",
       sizes: "180x180",
-      href: "/icons/apple-touch-icon.png"
+      href: "/apple-touch-icon.png"
     }
   ],
   manifest: "/manifest.json"
@@ -28,9 +27,17 @@ export default function RootLayout({
   return (
     <NextAuthSessionProvider>
       <html lang="en">
-        <Head>
+        <head>
           <link rel="manifest" href="/manifest.json" />
-        </Head>
+          <link rel="icon" sizes="192x192" href="/icons/icon-192x192.png" />
+          <link rel="icon" sizes="512x512" href="/icon-512x512.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <meta name="theme-color" content="#000000" />
+        </head>
         <body>
           <Navbar />
           {children}
